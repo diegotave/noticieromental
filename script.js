a991b660eeb58e27282c193195dfed4516748d99
diegotave <diegotavella@gmail.com>
2026-03-06 22:35:50 -0300

    toodo

---
+++ script.js
@@ -0,0 +1,14 @@
+html, body {
+  margin: 0;
+  width: 100%;
+  height: 100%;
+  background: black;
+  overflow: hidden;
+}
+
+model-viewer {
+  width: 100%;
+  height: 100%;
+  background: yellow;
+  display: block;
+}
